e184079a6952440da6f5dc624890bd1d9b5b20bf
Dariusz Okoński <darek200180@gmail.com>
2024-10-23 16:48:35 +0200

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
