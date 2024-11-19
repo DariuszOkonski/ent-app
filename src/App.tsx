9a65b413e2bab661beb9e204e976a8aaa7575f31
Dariusz Okoński <darek200180@gmail.com>
2024-11-19 17:48:19 +0100

    add temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
 import ExpenseForm from './expense-tracker/components/ExpenseForm';
 import ExpenseList from './expense-tracker/components/ExpenseList';
 
-function App() {
+function AppTemp() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 11, category: 'Utilities' },
@@ -35,6 +35,27 @@ function App() {
         expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
+    </div>
+  );
+}
+
+function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // side effect
+    if (ref.current) {
+      ref.current.focus();
+    }
+  });
+
+  useEffect(() => {
+    document.title = 'My App';
+  });
+
+  return (
+    <div>
+      <input ref={ref} type='text' className='form-control' />
     </div>
   );
 }
