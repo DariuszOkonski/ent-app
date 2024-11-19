8c81f7679427e0e7cb12904308ea979c3388a212
Dariusz Okoński <darek200180@gmail.com>
2024-11-19 18:10:22 +0100

    cleanup from useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -40,22 +40,17 @@ function AppTemp() {
   );
 }
 
-function App() {
-  const [category, setCategory] = useState('');
+const connect = () => console.log('Connecting');
+const disconnect = () => console.log('Disconnecting');
 
-  return (
-    <div>
-      <select
-        className='form-select'
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=''></option>
-        <option value='Clothing'>Clothing</option>
-        <option value='Household'>Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+function App() {
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  }, []);
+
+  return <div>App</div>;
 }
 
 export default App;
